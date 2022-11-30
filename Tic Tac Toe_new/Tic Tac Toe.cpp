4588c697f16e18141833372dc8862823f4f9fb93
scenicsystems <mattvariety@gmail.com>
2022-11-30 14:23:13 -0600

    Started refactoring to OOP-based paradigm, classes based in ttt_structs.h. Currently, there is an issue with marking squares on the board, as no effect is made after player input; will investigate further.

--- Tic Tac Toe_new/Tic Tac Toe.cpp
+++ Tic Tac Toe_new/Tic Tac Toe.cpp
@@ -5,216 +5,6 @@
 #include "ttt_structs.h"
 
 
-void printSquare(const Square& square)
-{
-	switch (square.status)
-	{
-	case (Players::noPlayer):
-	{
-		std::cout << "[ ]";
-		return;
-	}
-	case (Players::player1):
-	{
-		std::cout << "[X]";
-		return;
-	}
-	case (Players::player2):
-	{
-		std::cout << "[O]";
-		return;
-	}
-	default:
-	{
-		std::cout << "[?]";
-		return;
-	}
-	}
-
-	return;
-}
-
-void printLine(const Line& line)
-{
-	for (int i{ 0 }; i < maxColumns; i++)
-	{
-		printSquare(line.s[i]);
-		std::cout << " ";
-	}
-}
-
-void printBoard(const Board& board)
-{
-	for (int i{ 0 }; i < maxColumns; i++)
-	{
-		std::cout << alphabet[i] << "\t";
-		printLine(board.l[i]);
-		std::cout << "\n";
-	}
-
-	std::cout << "\t ";
-
-	for (int i(0); i < maxRows; i++)
-	{
-		std::cout << (i + 1) << "   ";
-	}
-
-	std::cout << "\n\n";
-}
-
-Line makeTestLine()
-{
-	Line testLine{  };
-	testLine.s[1].status = Players::player1;
-	testLine.s[2].status = Players::player2;
-	return testLine;
-}
-
-std::string getPlayerName(Players player)
-{
-	auto playerNum{ std::to_string(static_cast<int>(player)) };
-	std::string playerName{ "Player " };
-	playerName += playerNum;
-	return playerName;
-}
-
-void markSquare(Board& board, Players player)
-{
-	while (true)
-	{
-		std::cout << getPlayerName(player) << ", enter a row (a - " << alphabet[(maxRows - 1)] << ") and column (1 - " << (maxColumns) << "): ";
-
-		char rowInput{};
-		std::cin >> rowInput;
-
-		// Finds char in global alphabet array and returns index in int form (from 0 - 26)
-		int row{ static_cast<int>(alphabet.find(rowInput)) };
-
-		int columnInput{};
-		std::cin >> columnInput;
-
-		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-
-		if (!std::cin) // if cin extraction fails
-		{
-			std::cin.clear();
-			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-		}
-
-		// Since arrays in C++ start at index 0, this decrements the input to fit the line array
-		--columnInput;
-
-		if (columnInput >= maxColumns || columnInput < 0)
-			std::cout << "Invalid column, please enter a column between 1 and " << maxColumns << ".\n";
-		else if (row == -1 || row >= maxRows)
-			std::cout << "Invalid row, please enter a row between a and " << alphabet[(maxRows - 1)] << ".\n";
-		else if (board.l[row].s[columnInput].status != Players::noPlayer)
-			std::cout << "This square has already been marked by " << getPlayerName(board.l[row].s[columnInput].status) << "!\n";
-		else {
-			board.l[row].s[columnInput].status = player;
-			return;
-		}
-
-	}
-
-}
-
-Players checkForLineFill(const Line& line)
-{
-	bool fill{ true };
-	Players currentPlayerFill{ line.s[0].status };
-	for (int i{ 0 }; i < maxColumns && fill; i++)
-	{
-		if (line.s[i].status == Players::noPlayer || line.s[i].status != currentPlayerFill)
-			fill = false;
-	}
-
-	if (fill)
-		return currentPlayerFill;
-	else
-		return Players::noPlayer;
-}
-
-Players verticalLineCheck(const Board& board)
-{
-	for (int i{ 0 }; i < maxColumns; i++)
-	{
-		// Create a temporary Line to check for fills
-		Line tempLine{};
-
-		// Fill temporary line with vertical squares on Board, row by row downwards
-		for (int n{ 0 }; n < maxRows; n++)
-		{
-			tempLine.s[n].status = board.l[n].s[i].status;
-		}
-
-		// Returns with the player who filled up the line if one was found
-		Players winCheck{ checkForLineFill(tempLine) };
-		if (winCheck != Players::noPlayer)
-			return winCheck;
-	}
-
-	// If no player has filled up a vertical line, returns noPlayer
-	return Players::noPlayer;
-}
-
-Players diagonalLineCheck(Board board)
-{
-	for (int i{ 0 }; i < maxColumns && i < maxRows; i++)
-	{
-		// Create a temporary Line to check for fills
-		Line tempLine{};
-
-		// Fill temporary line with diagonal squares on Board
-		// Tests for diagonal #1 (starts at top left, goes to bottom right)
-		for (int n{ 0 }; n < maxRows; n++)
-		{
-			tempLine.s[n].status = board.l[n].s[n].status;
-		}
-
-		Players winCheck{ checkForLineFill(tempLine) };
-		if (winCheck != Players::noPlayer)
-			return winCheck;
-
-		// Tests for diagonal #2 (starts at top right, goes to bottom left)
-		int x{ maxRows - 1 };
-
-		for (int n{ 0 }; n < maxRows; n++, x--)
-		{
-			tempLine.s[n].status = board.l[n].s[x].status;
-		}
-
-		winCheck = checkForLineFill(tempLine);
-		if (winCheck != Players::noPlayer)
-			return winCheck;
-
-	}
-
-	return Players::noPlayer;
-
-}
-
-Players checkForWin(const Board& board)
-{
-	// Check for horizontal fills
-	for (int i{ 0 }; i < maxRows; i++)
-	{
-		Players winCheck{ checkForLineFill(board.l[i]) };
-		if (winCheck != Players::noPlayer)
-			return winCheck;
-	}
-
-	// Check for vertical or diagonal fills; if none found, returns noPlayer
-	Players winCheck{};
-	if ((winCheck = verticalLineCheck(board)) != Players::noPlayer)
-		return winCheck;
-	else if ((winCheck = diagonalLineCheck(board)) != Players::noPlayer)
-		return winCheck;
-	else
-		return Players::noPlayer;
-
-}
-
 bool playAgain()
 {
 	while (true)
@@ -247,13 +37,13 @@ Players playGame(Board board)
 
 	for (; winner == Players::noPlayer && turn <= (maxColumns * maxRows); turn++)
 	{
-		printBoard(board);
+		board.print();
 		if (turn % 2 != 0)
-			markSquare(board, Players::player1);
+			board.markSquare(Players::player1);
 		else
-			markSquare(board, Players::player2);
+			board.markSquare(Players::player2);
 
-		winner = checkForWin(board);
+		winner = board.checkForWin();
 	}
 
 	return winner;
@@ -273,7 +63,7 @@ int main()
 
 		std::cout << "\n\n";
 
-		printBoard(gameBoard);
+		gameBoard.print();
 	} while (playAgain());
 
 
--- Tic Tac Toe_new/ttt_structs.h
+++ Tic Tac Toe_new/ttt_structs.h
@@ -1,6 +1,7 @@
 #ifndef TTT_STRUCTS
 #define TTT_STRUCTS
 #include <string_view>
+#include <iostream>
 
 
 constexpr int maxRows{ 5 };
@@ -15,20 +16,261 @@ enum class Players
 	player2,
 };
 
-struct Square
+std::string getPlayerName(Players player)
 {
-	Players status{ Players::noPlayer };
+	auto playerNum{ std::to_string(static_cast<int>(player)) };
+	std::string playerName{ "Player " };
+	playerName += playerNum;
+	return playerName;
+}
+
+class Square
+{
+private:
+	Players m_status{ };
+
+public:
+
+	Square() : m_status{ Players::noPlayer }
+	{
+
+	}
+
+	Players getStatus()
+	{
+		return m_status;
+	}
+
+	void setStatus(Players player)
+	{
+		std::cout << "\nDEBUG: before set, player status: " << getPlayerName(getStatus());
+		m_status = player;
+		std::cout << "\nDEBUG: after set, player status: " << getPlayerName(getStatus());
+	}
+
+	void print()
+	{
+		switch (m_status)
+		{
+		case (Players::noPlayer):
+		{
+			std::cout << "[ ]";
+			return;
+		}
+		case (Players::player1):
+		{
+			std::cout << "[X]";
+			return;
+		}
+		case (Players::player2):
+		{
+			std::cout << "[O]";
+			return;
+		}
+		default:
+		{
+			std::cout << "[?]";
+			return;
+		}
+		}
+
+		return;
+	}
 
 };
 
-struct Line
+class Line
 {
-	Square s[maxRows]{};
+private:
+	Square m_square[maxRows]{};
+
+public:
+
+	Line() : m_square{}
+	{
+	}
+
+	Square getSquare(int squareNum)
+	{
+		return m_square[squareNum];
+	}
+
+	void print()
+	{
+		for (int i{ 0 }; i < maxColumns; i++)
+		{
+			m_square[i].print();
+			std::cout << " ";
+		}
+	}
+
+	Players checkForFill()
+	{
+		bool fill{ true };
+		Players currentPlayerFill{ m_square[0].getStatus()};
+		for (int i{ 0 }; i < maxColumns && fill; i++)
+		{
+			if (m_square[i].getStatus() == Players::noPlayer || m_square[i].getStatus() != currentPlayerFill)
+				fill = false;
+		}
+
+		if (fill)
+			return currentPlayerFill;
+		else
+			return Players::noPlayer;
+	}
+
 };
 
-struct Board
+
+
+class Board
 {
-	Line l[maxColumns]{};
+private:
+	Line m_line[maxColumns]{};
+
+public:
+
+	void print()
+	{
+		for (int i{ 0 }; i < maxColumns; i++)
+		{
+			std::cout << alphabet[i] << "\t";
+			m_line[i].print();
+			std::cout << "\n";
+		}
+
+		std::cout << "\t ";
+
+		for (int i(0); i < maxRows; i++)
+		{
+			std::cout << (i + 1) << "   ";
+		}
+
+		std::cout << "\n\n";
+	}
+
+	void markSquare(Players player)
+	{
+		while (true)
+		{
+			std::cout << getPlayerName(player) << ", enter a row (a - " << alphabet[(maxRows - 1)] << ") and column (1 - " << (maxColumns) << "): ";
+
+			char rowInput{};
+			std::cin >> rowInput;
+
+			// Finds char in global alphabet array and returns index in int form (from 0 - 26)
+			int row{ static_cast<int>(alphabet.find(rowInput)) };
+
+			int columnInput{};
+			std::cin >> columnInput;
+
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+			if (!std::cin) // if cin extraction fails
+			{
+				std::cin.clear();
+				std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			}
+
+			// Since arrays in C++ start at index 0, this decrements the input to fit the line array
+			--columnInput;
+
+			if (columnInput >= maxColumns || columnInput < 0)
+				std::cout << "Invalid column, please enter a column between 1 and " << maxColumns << ".\n";
+			else if (row == -1 || row >= maxRows)
+				std::cout << "Invalid row, please enter a row between a and " << alphabet[(maxRows - 1)] << ".\n";
+			else if (m_line[row].getSquare(columnInput).getStatus() != Players::noPlayer)
+				std::cout << "This square has already been marked by " << getPlayerName(m_line[row].getSquare(columnInput).getStatus()) << "!\n";
+			else {
+				m_line[row].getSquare(columnInput).setStatus(player);
+				return;
+			}
+
+		}
+
+	}
+
+	Players verticalLineCheck()
+	{
+		for (int i{ 0 }; i < maxColumns; i++)
+		{
+			// Create a temporary Line to check for fills
+			Line tempLine{};
+
+			// Fill temporary line with vertical squares on Board, row by row downwards
+			for (int n{ 0 }; n < maxRows; n++)
+			{
+				tempLine.getSquare(n).setStatus(m_line[n].getSquare(i).getStatus());
+			}
+
+			// Returns with the player who filled up the line if one was found
+			Players winCheck{ tempLine.checkForFill()};
+			if (winCheck != Players::noPlayer)
+				return winCheck;
+		}
+
+		// If no player has filled up a vertical line, returns noPlayer
+		return Players::noPlayer;
+	}
+
+	Players diagonalLineCheck()
+	{
+		for (int i{ 0 }; i < maxColumns && i < maxRows; i++)
+		{
+			// Create a temporary Line to check for fills
+			Line tempLine{};
+
+			// Fill temporary line with diagonal squares on Board
+			// Tests for diagonal #1 (starts at top left, goes to bottom right)
+			for (int n{ 0 }; n < maxRows; n++)
+			{
+				tempLine.getSquare(n).setStatus(m_line[n].getSquare(n).getStatus());
+			}
+
+			Players winCheck{ tempLine.checkForFill()};
+			if (winCheck != Players::noPlayer)
+				return winCheck;
+
+			// Tests for diagonal #2 (starts at top right, goes to bottom left)
+			int x{ maxRows - 1 };
+
+			for (int n{ 0 }; n < maxRows; n++, x--)
+			{
+				tempLine.getSquare(n).setStatus(m_line[n].getSquare(x).getStatus());
+			}
+
+			winCheck = tempLine.checkForFill();
+			if (winCheck != Players::noPlayer)
+				return winCheck;
+
+		}
+
+		return Players::noPlayer;
+
+	}
+
+	Players checkForWin()
+	{
+		// Check for horizontal fills
+		for (int i{ 0 }; i < maxRows; i++)
+		{
+			Players winCheck{ m_line[i].checkForFill()};
+			if (winCheck != Players::noPlayer)
+				return winCheck;
+		}
+
+		// Check for vertical or diagonal fills; if none found, returns noPlayer
+		Players winCheck{};
+		if ((winCheck = this->verticalLineCheck()) != Players::noPlayer)
+			return winCheck;
+		else if ((winCheck = this->diagonalLineCheck()) != Players::noPlayer)
+			return winCheck;
+		else
+			return Players::noPlayer;
+
+	}
+
 };
 
 #endif
